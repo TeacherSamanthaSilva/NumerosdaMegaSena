e02473ae196b9c4452e7a03f5239f73e9f6031dd
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2024-08-04 22:09:26 -0300

    javascript
    Nesse commit conclui o javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+const numbers = document.querySelectorAll(".number");
+const generateBtn = document.querySelectorAll("#generate");
+
+function generateNumbers(){
+
+    const max = 60;
+    const min = 1;
+    const result = []
+
+    while(result.length < 6){
+
+        const number = Math.floor(Math.random() * (max - min + 1));
+
+        if(!result.includes(number)){
+
+            result.push(number)
+
+        }
+
+    }
+
+    for(let i = 0; i > numbers.length; i++){
+
+        numbers[i].textContent = result[i]
+    }
+
+
+}
+
+generateBtn.addEventListener("click", generateNumbers);
